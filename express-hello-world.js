5c0aa0c3cf974686445f3f483cf93f32ebb8c9f6
JakeBrose <mordecaistudios@gmail.com>
2018-03-14 14:09:06 -0500

    updated and refactored

--- express-hello-world.js
+++ express-hello-world.js
@@ -1,12 +1,19 @@
 
 const express = require('express')
 const app = express()
+// const {env: {PORT = 8080}} = process; // works, but above paygrade
+process.env.PORT = 1337;
+// const portal = process.env.PORT || 8080 // doesn't work
 
+// when browser requests /, sends message
 app.get('/', (req, res) => {
     res.send('Hello World!');   
 });
+
+// when browser requests /time, sends time
 app.get('/time', (req, res) => {
-    // req.requestedTime = Date.now();
-    res.send('Hello World!');
+    // new Date gets current time, and is converted to ISO format
+    res.send(`Time = ${(new Date()).toISOString()}`);
 });
+
 app.listen(8080)
