7376e364e174b26c682d4906b491141e63db5b5c
Jake Brose <33636853+JakeBrose@users.noreply.github.com>
2018-04-17 14:46:44 -0500

    Update express-hello-world.js
    Removed commented out code

--- express-hello-world.js
+++ express-hello-world.js
@@ -1,9 +1,7 @@
 
 const express = require('express')
 const app = express()
-// const {env: {PORT = 8080}} = process; // works, but above paygrade
 process.env.PORT = 1337;
-// const portal = process.env.PORT || 8080 // doesn't work
 
 // when browser requests /, sends message
 app.get('/', (req, res) => {
